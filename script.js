dc3d79a830a43b6aeefb5d2d353bb5947dc93afb
piyush22F <piyushchaube12@gmail.com>
2024-04-06 01:44:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,169 @@
+let crsr = document.querySelector(".cursor");
+let blur = document.querySelector(".cursor-blur");
+
+document.addEventListener("mousemove", (dets) => {
+  crsr.style.left = dets.x - "10" + "px";
+  crsr.style.top = dets.y - "10" + "px";
+  blur.style.left = dets.x - "175" + "px";
+  blur.style.top = dets.y - "175" + "px";
+});
+
+let img = document.querySelector("nav img");
+
+img.addEventListener("mouseenter", () => {
+  crsr.style.width = "70px";
+  crsr.style.height = "70px";
+  crsr.style.transform = "translate(-50%,-50%)";
+  crsr.style.backgroundColor = "transparent";
+  crsr.style.border = "2px solid #fff";
+});
+
+img.addEventListener("mouseleave", () => {
+  crsr.style.width = "20px";
+  crsr.style.height = "20px";
+  crsr.style.backgroundColor = "rgb(149 193 30)";
+  crsr.style.border = "none";
+  crsr.style.transform = "none";
+});
+
+let aTags = document.querySelectorAll("nav a");
+
+aTags.forEach(function (elem) {
+  elem.addEventListener("mouseenter", () => {
+    crsr.style.width = "70px";
+    crsr.style.height = "70px";
+    crsr.style.transform = "translate(-50%,-50%)";
+    crsr.style.backgroundColor = "transparent";
+    crsr.style.border = "2px solid #fff";
+  });
+  elem.addEventListener("mouseleave", () => {
+    crsr.style.width = "20px";
+    crsr.style.height = "20px";
+    crsr.style.backgroundColor = "rgb(149 193 30)";
+    crsr.style.border = "none";
+    crsr.style.transform = "none";
+  });
+});
+
+let Tags= document.querySelectorAll(".footer-1 a")
+console.log(Tags);
+
+
+
+
+
+
+gsap.to("nav", {
+  backgroundColor: "black",
+  duration: 1,
+  height: "110px",
+  scrollTrigger: {
+    trigger: "nav",
+    scroller: "body",
+    // markers: true,
+    start: "top -10",
+    end: "top -20",
+    scrub: 1,
+  },
+});
+
+gsap.to(".main", {
+  backgroundColor: "black",
+  scrollTrigger: {
+    trigger: ".main",
+    scroller: "body",
+    // markers: true,
+    start: "top -30%",
+    end: "top -65%",
+    scrub: 2,
+  },
+});
+
+gsap.from(".about h1", {
+  transform: "translateY(50%)",
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".about h1",
+    scroller: "body",
+    // markers: true,
+    start: "top 80%",
+    end: "top 70%",
+    scrub: 1,
+  },
+});
+
+gsap.from(".about .center", {
+  transform: "translateY(50%)",
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".about .center",
+    scroller: "body",
+    // markers: true,
+    start: "top 80%",
+    end: "top 70%",
+    scrub: 1,
+  },
+});
+
+gsap.from(".center img", {
+  transform: "translateY(50%)",
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".center img",
+    scroller: "body",
+    // markers: true,
+    start: "top 80%",
+    end: "top 70%",
+    scrub: 1,
+  },
+});
+
+gsap.from(".card", {
+  transform: "translateY(50%)",
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".card",
+    scroller: "body",
+    // markers: true,
+    start: "top 80%",
+    end: "top 70%",
+    scrub: 1,
+    stragger: 1,
+  },
+});
+
+gsap.from(".page4 #img1", {
+  transform: "translate(100px, 0px)",
+  scrollTrigger: {
+    trigger: ".page4",
+    scroller: "body",
+    // markers: true,
+    start: "top 70%",
+    end: "top 40%",
+    scrub: 1,
+  },
+});
+
+gsap.from(".page4 #img2", {
+  transform: "translate(-100px, 0px)",
+  scrollTrigger: {
+    trigger: ".page4",
+    scroller: "body",
+    // markers: true,
+    start: "top 70%",
+    end: "top 40%",
+    scrub: 1,
+  },
+});
+
+gsap.from(".page4-bottom h2", {
+  transform: "translateY(100px)",
+  scrollTrigger: {
+    trigger: ".page4-bottom",
+    scroller: "body",
+    // markers: true,
+    start: "top 70%",
+    end: "top 50%",
+    scrub: 1,
+  },
+});
